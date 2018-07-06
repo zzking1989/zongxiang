44ec3aefcddaf5fa1840a6e780359ae89ea9ae07
tendoria <zzking1989@163.com>
2018-07-07 00:04:50 +0800

    delete get set

--- src/main/java/com/zx/utils/ZongXiangResult.java
+++ src/main/java/com/zx/utils/ZongXiangResult.java
@@ -1,10 +1,16 @@
 package com.zx.utils;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.omg.CORBA.PUBLIC_MEMBER;
 
 import java.io.Serializable;
 
-public class ZongXiangResult  {
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ZongXiangResult {
     // 响应业务状态
     private Integer code1;
 
@@ -19,25 +25,6 @@ public class ZongXiangResult  {
         return code1;
     }
 
-    public void setCode1(Integer code1) {
-        this.code1 = code1;
-    }
-
-    public String getName1() {
-        return name1;
-    }
-
-    public void setName1(String name1) {
-        this.name1 = name1;
-    }
-
-    public Object getData1() {
-        return data1;
-    }
-
-    public void setData1(Object data1) {
-        this.data1 = data1;
-    }
 
     @Override
     public String toString() {
